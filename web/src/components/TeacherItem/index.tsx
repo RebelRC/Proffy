2bd0cc8105462e8d88ee3518967c2bb5b89a6725
Rafael Rebel <rafaelcrebel@gmail.com>
2020-08-06 15:37:55 -0300

    Dia 3 - Integração do backend com frontend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,52 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+};
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+    
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/63623829?s=460&u=9f6eb118ecd681b5d3ef99fed1c999f0164152ef&v=4.com/account" alt="Rafael Rebel"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Rafael Rebel</strong>
-                    <span>Engenharia</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Engenheiro mecânico, cursando engenharia de computação.
-                <br /><br />
-                Procurando seu lugar no mercado de trabalho. Vê seu trabalho na Aurea como uma oportunidade de se desenvolver profissionalmente e alcançar seus objetivos.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 40,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={'https://wa.me/${teacher.whatsapp}'}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato 
-                </button>
+                </a>
             </footer>
         </article>
     );
